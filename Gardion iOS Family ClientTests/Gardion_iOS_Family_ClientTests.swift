432a75a3ba6c963aafa3bdf5a23791590ea57c2d
Tom Schlenkhoff <ts@gardion.com>
2018-06-24 17:04:46 +0200

    Initial commit

---
+++ Gardion iOS Family ClientTests/Gardion_iOS_Family_ClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gardion_iOS_Family_ClientTests.swift
+//  Gardion iOS Family ClientTests
+//
+//  Created by Tom on 24.06.18.
+//  Copyright © 2018 Gardion. All rights reserved.
+//
+
+import XCTest
+@testable import Gardion_iOS_Family_Client
+
+class Gardion_iOS_Family_ClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
